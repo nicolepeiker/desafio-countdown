dc9909677f08860bd4f2c861895fef5a1e112af0
nicolepeiker <nicolervpeiker@gmail.com>
2022-03-12 15:36:48 -0300

    alteração z-index

--- script.js
+++ script.js
@@ -2,33 +2,32 @@ const countdown = () => {
   const finalTime = new Date('January 01, 2023 00:00:00').getTime() //pega a data desejada e getTime transforma em milisegundos
   const now = new Date().getTime()
   const gap = finalTime - now
-  
-  const day = 1000*60*60*24
-  const hour = 1000*60*60
-  const minute = 1000*60
+
+  const day = 1000 * 60 * 60 * 24
+  const hour = 1000 * 60 * 60
+  const minute = 1000 * 60
   const second = 1000
-  
+
   const countDay = gap / day
-  const countHour = (gap%day) / hour
-  const countMinute = (gap%hour) / minute
-  const countSecond = (gap%minute) / second
-  
+  const countHour = (gap % day) / hour
+  const countMinute = (gap % hour) / minute
+  const countSecond = (gap % minute) / second
+
   const getDay = document.querySelector('#days')
   const getHour = document.querySelector('#hours')
   const getMinute = document.querySelector('#minutes')
   const getSecond = document.querySelector('#seconds')
-  
+
   getDay.innerText = countDay.toFixed()
   getHour.innerText = countHour.toFixed()
   getMinute.innerText = countMinute.toFixed()
   getSecond.innerText = countSecond.toFixed()
 }
 
-setInterval(countdown,1000)
+setInterval(countdown, 1000)
 
 const openModal = document.querySelector('#openModal')
-openModal.addEventListener('click', function(){
-  const modal = document.querySelector('.modal') 
+openModal.addEventListener('click', function () {
+  const modal = document.querySelector('.modal')
   modal.classList.add('active')
 })
-
